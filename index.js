f34fc45a38eb8f570e8126862469b6206a9c44f8
jennawilkinson <43350731+jennawilkinson@users.noreply.github.com>
2020-03-13 13:23:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.json())
 //Serve ‘public’ folder as static website
 app.use( express.static('public') )
 
-app.get("/items", (req, res) => {
-    let sql = `SELECT * FROM items`;
+app.get("/songs", (req, res) => {
+    let sql = `SELECT * FROM songs`;
 
     db.all(sql, [], (err, rows) => {
         if (err) {
@@ -22,17 +22,17 @@ app.get("/items", (req, res) => {
         rows.forEach((row) => {
           //nothing for each row
         });
-        console.log("GET /items - items sent to user");
+        console.log("GET /songs - songs sent to user");
         res.send(rows);
     });
 })
 
-app.post("/login", (req, res) => {
+app.post("/createAccount", (req, res) => {
     const user = req.body.user
     const sql = `INSERT INTO users 
-    (firstname, lastname, email, password) 
+    (firstName, lastName, email, password) 
     VALUES (?, ?, ?, ?)`
-    const values = [user.first, user.last, user.email, user.password]
+    const values = [user.firstName, user.lastName, user.email, user.password]
     let userID
     db.run(sql, values, function (err) {
         if (err)
@@ -43,10 +43,65 @@ app.post("/login", (req, res) => {
     })
     
     res.json({
-        message: 'User logged in',
+        message: 'User account created',
         userID: userID 
+    })
+})
+
+
+app.post("/login", (req, res) => {
+    const user = req.body.user
+    const sql = `SELECT userid FROM users WHERE password = ? AND email = ?`
+    const values = [user.first, user.last, user.email, user.password]
+    let userID
+    db.all(sql, values, (err, rows) => {
+        //rows is an array of records from SQL query
+        if (err) {
+            throw err;
+        }
+       // else {            
+       //     console.log(`userid ${this.lastID} created`)
+        //}
+        if (rows.length == 0) {
+            res.status(404)
+        res.json({
+            message: 'No such user',
+            userID: null
+            })
+        }
+                 
+        else {
+            res.status(200)
+        res.json({
+            message: 'User logged in',
+            userID: rows[0].userID 
+            })
+        }
+    });  
+})
+
+app.post("/addsong", (req, res) => {
+    const song = req.body.song
+    const sql = `INSERT INTO songs
+    (name, length, date, artist, genre) 
+    VALUES (?, ?, ?, ?, ?)`
+    const values = [song.name, song.length, song.date, song.artist, song.genre]
+    let songID
+    db.run(sql, values, function (err) {
+        if (err)
+            console.log(err)
+        else {            
+            console.log(`songid ${this.lastID} created`)
+        }
+    })
+    
+    res.json({
+        message: 'User added song',
+        songID: songID
     })
 })
 
 //Listens for web requests
 app.listen(80, () => console.log("Server started") )
+
+
